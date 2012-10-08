a292da9b512089b3a14449278e68b6d6b03b8acb
Jesper Weissglas <jesper@journeyman.se>
2012-10-08 18:14:25 -0200

    Adjust position instr size

--- plugins/dashboard_pi/src/instrument.cpp
+++ plugins/dashboard_pi/src/instrument.cpp
@@ -206,7 +206,7 @@ wxSize DashboardInstrument_Position::GetSize()
       wxClientDC dc(this);
       int w;
       dc.GetTextExtent(m_title, &w, &m_TitleHeight, 0, 0, g_pFontTitle);
-      dc.GetTextExtent(_T("000  00.0000"), &w, &m_DataHeight, 0, 0, g_pFontData);
+      dc.GetTextExtent(_T("000  00.0000 W"), &w, &m_DataHeight, 0, 0, g_pFontData);
 
       if( ((wxBoxSizer *)(GetParent()->GetSizer()))->GetOrientation() == wxHORIZONTAL ) {
           return wxSize( w+10, m_TitleHeight+m_DataHeight*2 );
